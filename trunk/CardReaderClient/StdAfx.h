8e73aa39db4c117f9f268c2a9cee3102236aa29c
icejoywoo <icejoywoo@e064f019-fdb2-f8e4-89af-ef8ae8e96563>
2011-11-28 09:58:28 +0000

    工作结束, 最后提交一下

--- trunk/CardReaderClient/StdAfx.h
+++ trunk/CardReaderClient/StdAfx.h
@@ -20,6 +20,8 @@
 #include <afxcmn.h>			// MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
+// my own header files
+#include "SimpleLog.h"
 
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
