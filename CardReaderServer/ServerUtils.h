97393180d566ec6d028166e05f3557f046a7118f
icejoywoo <icejoywoo@e064f019-fdb2-f8e4-89af-ef8ae8e96563>
2011-12-04 07:00:07 +0000

    周六的工作提交

--- CardReaderServer/ServerUtils.h
+++ CardReaderServer/ServerUtils.h
@@ -3,6 +3,7 @@
 
 #include "StdAfx.h"
 #include "SplitStr.h"
+#include "SmartComString.h"
 
 /**
  * @brief 将int转换为CString
@@ -62,18 +63,31 @@ CString getIpAdress(CIPAddressCtrl& ip);
  * @brief 解析网络命令, 对读卡器进行相应的操作
  * @param 
  *	in command 命令
- *	out operationName 操作名称, 下面是对照表
- *		操作名称			对应操作			SmartCom函数
+ *	out operationName 操作名称, 下面是operationName对照表
+ *		*操作名称*			*对应操作*			*SmartCom函数*
  *		reset				复位操作			ResetDev
  *		shutdown			卡片下电			ShutDownCard
  *		clearMemory			擦除存储器			ClearMem
  *		modifyBraudRate		修改波特率			ModifyCardBraudRate
- 
- *		shutdown			卡片下电			ShutDownCard
- *		shutdown			卡片下电			ShutDownCard
- *		shutdown			卡片下电			ShutDownCard
- *		shutdown			卡片下电			ShutDownCard
- * @return 成功为0, 失败为其他, 具体参考SmartCom和operationName
+ *		downloadFile		下载文件			DownloadFile
+ *		cardApdu			执行APDU命令		CardApdu
+ *		executeMulApdu		执行APDU批处理		ExcuteMulAPDU
+ *		getScript			读取脚本文件		GetScriptData
+ *		checkBatchResult	批处理APDU结果		CheckBatchResult
+ *		modifyCardPower		修改卡片电源		ModifyCardPower
+ *		getDevIdAndMacNo	获取设备id和机号	GetDevIdAndMacNo
+ *		setMacNoByDevID		设置设备id和机号	SetMacNoByDevID
+ *		getAppVerAndDevType	读取程序版本和终端类型		GetAppVerAndDevType
+ *		getChipID			获取读卡接口芯片ID号		GetChipID
+ *		isCardReady			检测A卡和B卡座是否有卡		IsCardReady
+ *		resetDev			卡片复位应答		ResetDev
+ * @return 
+ *	成功为0
+ *	部分错误码:
+ *		-100, 命令找不到
+ *		-101, 与卡片读写器的通信初始化失败
+ *		-102, 关闭udp通信失败
+ *	失败为其他, 具体参考SmartCom和operationName对照表
  */
 int parseCommand(SOCKET client, char* command, CString& operationName);
 
@@ -85,14 +99,7 @@ int parseCommand(SOCKET client, char* command, CString& operationName);
  * @return 发送的字符数
  */
 int sendData(SOCKET s, int data);
-
-/**
- * @brief 通过socket发送数据
- * @param 
- *	in s 需要发送数据的socket
- *	in data 需要发送的数据
- * @return 发送的字符数
- */
-int sendData(SOCKET s, char* data);
+int sendData(SOCKET s, const char* data);
+int sendData(SOCKET s, SmartCom::string data);
 
 #endif
