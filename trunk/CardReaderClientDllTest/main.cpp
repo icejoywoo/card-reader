9e9348e9a1be4d02ef2a3400c06224569dac50d1
icejoywoo <icejoywoo@e064f019-fdb2-f8e4-89af-ef8ae8e96563>
2011-12-12 13:01:29 +0000

    客户端dll基本完成, 未全部测试

--- trunk/CardReaderClientDllTest/main.cpp
+++ trunk/CardReaderClientDllTest/main.cpp
@@ -49,17 +49,18 @@ int main(void)
 	{
 		printf("getReader Failed.");
 	}
-	char devID[512];
-	int readerId;
+
 
 	// 获取读卡器id
-// 	{
-// 		if (0 != GetDevIDAndReaderId(reader, devID, 512, readerId))
-// 		{
-// 			printf("GetDevIDAndReaderId Failed.");
-// 		}
-// 		cout << devID << ", " << readerId << endl;
-// 	}
+	{
+		char devID[512];
+		int readerId;
+		if (0 != GetDevIDAndReaderId(reader, devID, 512, readerId))
+		{
+			printf("GetDevIDAndReaderId Failed.");
+		}
+		cout << devID << ", " << readerId << endl;
+	}
 	
 	// 修改读卡器id
 // 	{
@@ -79,40 +80,40 @@ int main(void)
 // 	}
 	
 	// 读取程序版本和终端类型
-// 	char appVer[512];
-// 	char devType[512];
-// 	{
-// 		if (0 != GetAppVerAndDevType(reader, appVer, 512, devType, 512))
-// 		{
-// 			printf("GetAppVerAndDevType Failed.");
-// 		}
-// 		cout << appVer << ", " << devType << endl;
-// 	}
+	{
+		char appVer[512];
+		char devType[512];
+		if (0 != GetAppVerAndDevType(reader, appVer, 512, devType, 512))
+		{
+			printf("GetAppVerAndDevType Failed.");
+		}
+		cout << appVer << ", " << devType << endl;
+	}
 
 	// 获取芯片id(调用复位之后调用这个, 会出错)
-// 	char chipID[512];
-// 	{
-// 		if (0 != GetChipID(reader, chipID, 512))
-// 		{
-// 			printf("GetChipID failed.");
-// 		}
-// 		cout << chipID << endl;
-// 	}
+	{
+		char chipID[512];
+		if (0 != GetChipID(reader, chipID, 512))
+		{
+			printf("GetChipID failed.");
+		}
+		cout << chipID << endl;
+	}
 
 	// 检测A卡和B卡座是否有卡
-// 	int cardA;
-// 	int cardB;
-// 	{
-// 		if (0 != IsCardReady(reader, cardA, cardB))
-// 		{
-// 			printf("IsCardReady failed.");
-// 		}
-// 		cout << cardA << ", " << cardB << endl;
-// 	}
+	{
+		int cardA;
+		int cardB;
+		if (0 != IsCardReady(reader, cardA, cardB))
+		{
+			printf("IsCardReady failed.");
+		}
+		cout << cardA << ", " << cardB << endl;
+	}
 	
 	// 复位应答
-	SmartCom::string retCode;
 	{
+		SmartCom::string retCode;
 		if (0 != ResetCard(reader, retCode))
 		{
 			printf("ResetCard failed.");
@@ -121,14 +122,65 @@ int main(void)
 	}
 
 	// 执行单条命令(执行前需要先复位应答)
-	char* apdu = "00820000083132333435363738";
 	{
+		SmartCom::string retCode;
+		char* apdu = "00820000083132333435363738";
 		if (0 != CardApdu(reader, apdu, retCode, 1))
 		{
 			printf("CardApdu failed.");
 		}
 		cout << retCode.c_str() << endl;
 	}
+
+	// 修改波特率
+// 	{
+// 		if (0 != ModifyCardBraudRate(reader, 1))
+// 		{
+// 			printf("ModifyCardBraudRate failed.");
+// 		}
+// 	}
+
+	// 查看波特率
+	{
+		int braudRate;
+		if (0 != GetCardBraudRate(reader, braudRate))
+		{
+			printf("ModifyCardBraudRate failed.");
+		}
+		cout << braudRate << endl;
+	}
+
+	// 修改电源
+	{
+		if (0 != ModifyCardPower(reader, 1))
+		{
+			printf("ModifyCardBraudRate failed.");
+		}
+	}
+
+	// 下载文件
+// 	{
+// 		if (0 != DownloadFile(reader, 2, "apdu_head.bin")) // 下载头文件
+// 		{
+// 			printf("下载头文件失败\n");
+// 		}
+// 		if (0 != DownloadFile(reader, 1, "apdu_body.bin")) // 下载体文件
+// 		{
+// 			printf("下载体文件失败\n");
+// 		}
+// 	}
+
+	// 卡片下电
+	{
+		if (0 != ShutdownCard(reader))
+		{
+			printf("ShutdownCard failed.");
+		}
+	}
+	
+
+
+
 
 	// 释放读卡器
 	if (0 != ReleaseReader(reader))
