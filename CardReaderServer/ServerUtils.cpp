8d68b6e636ec3e3b64590f805a16d31cc4500d7e
icejoywoo <icejoywoo@e064f019-fdb2-f8e4-89af-ef8ae8e96563>
2011-12-14 06:40:39 +0000

    修改一些程序上的逻辑错误 1.读卡器中的MacNo是用来做扩展, 现在用udp还是com方式都不需要使用,已全部使用默认的255广播方式 2.initUdpComm函数只需要在最开始调用一次, 用工作站来连接读卡器

--- CardReaderServer/ServerUtils.cpp
+++ CardReaderServer/ServerUtils.cpp
@@ -26,11 +26,7 @@ int parseCommand(SOCKET client, int readerId, char* command, CString& operationN
 
 	Communicator communicator; // 与读卡器通信的通信
 
-	if (InitUDPComm() == -1) {
-		AfxMessageBox("与卡片读写器的通信初始化失败");
-		SimpleLog::error("与卡片读写器的通信初始化失败");
-		return -101; // 与卡片读写器的通信初始化失败
-	}
+	
 
 
 	// readerId的含义, 表示读卡器相应的com号
@@ -55,28 +51,28 @@ int parseCommand(SOCKET client, int readerId, char* command, CString& operationN
 
 	// refer to SmartCom.h
 	if (operationName == CString("reset")) { // 复位操作
-		return ResetDev(communicator, readerId);
+		return ResetDev(communicator);
 	} else if (operationName == CString("shutdown")) { // 卡片下电
-		return ShutdownCard(communicator, readerId);
+		return ShutdownCard(communicator);
 	} else if (operationName == CString("clearMemory")) { // 对SST25VF016B存储器进行整片擦除
-		return ClearMem(communicator, readerId);
+		return ClearMem(communicator);
 	} else if (operationName == CString("modifyBraudRate")) { // 修改比特率
 		int braudRate = atoi(requestParam[1]);
-		return ModifyCardBraudRate(communicator, braudRate, readerId);
+		return ModifyCardBraudRate(communicator, braudRate);
 	} else if (operationName == CString("getBraudRate")) { // 获得比特率
 		int braudRate;
-		int resultCode = GetCardBraudRate(communicator, braudRate, readerId);
+		int resultCode = GetCardBraudRate(communicator, braudRate);
 		sendData(client, braudRate);
 		return resultCode;
 	} else if (operationName == CString("downloadFile")) { // 下载文件
 		int flag = atoi(requestParam[1]); // 1 下载命令体文件, 2 下载命令头文件
 		char* fileName = (LPSTR)(LPCTSTR)requestParam[2]; // 文件名
-		return DownloadFile(communicator, flag, fileName, readerId);
+		return DownloadFile(communicator, flag, fileName);
 	} else if (operationName == CString("cardApdu")) { // 卡片操作-执行APDU命令
 		char* apdu = (LPSTR)(LPCTSTR)requestParam[1]; // apdu命令
 		int card = atoi(requestParam[2]); // 1 为A卡, 2 为B卡
 		SmartCom::string retCode; // 输出参数, 执行apdu指令的返回值
-		int resultCode = CardApdu(communicator, apdu, retCode, card, readerId);
+		int resultCode = CardApdu(communicator, apdu, retCode, card);
 		sendData(client, retCode);
 		return resultCode;
 	} else if (operationName == CString("executeMulApdu")) { // 卡片操作-执行批处理APDU命令
@@ -87,7 +83,7 @@ int parseCommand(SOCKET client, int readerId, char* command, CString& operationN
 		int offset = atoi(requestParam[1]); // 读取数据的文件偏移地址
 		char bytes = (char) atoi(requestParam[2]); // 要读取的字节数
 		SmartCom::string strData; // 输出参数 读取的转换成十六进制字符串的数据
-		int resultCode = GetScriptData(communicator, offset, bytes, strData, readerId);
+		int resultCode = GetScriptData(communicator, offset, bytes, strData);
 		if (resultCode == 0) {
 			sendData(client, strData);
 		} else {
@@ -96,7 +92,7 @@ int parseCommand(SOCKET client, int readerId, char* command, CString& operationN
 		return resultCode;
 	} else if (operationName == CString("checkBatchResult")) { // 查询执行批处理APDU结果
 		SmartCom::string retCode; // 输出参数, 最后一条指令的返回值
-		int resultCode = CheckBatchResult(communicator, retCode, readerId);
+		int resultCode = CheckBatchResult(communicator, retCode);
 		if (resultCode > 0) {
 			sendData(client, retCode);
 		} else {
@@ -106,7 +102,7 @@ int parseCommand(SOCKET client, int readerId, char* command, CString& operationN
 	} else if (operationName == CString("modifyCardPower")) { // 修改卡片电源
 		int power = atoi(requestParam[1]); // 
 		int card = atoi(requestParam[2]);
-		return ModifyCardPower(communicator, power, card, readerId);
+		return ModifyCardPower(communicator, power, card);
 	} else if (operationName == CString("getDevIdAndReaderId")) { // 读取终端设备id号和机号
 		SmartCom::string devID; // out, 存放7字节id号
 		int macNo; // out, 1字节机号
@@ -124,24 +120,24 @@ int parseCommand(SOCKET client, int readerId, char* command, CString& operationN
 	} else if (operationName == CString("getAppVerAndDevType")) { // 读取程序版本和终端类型
 		SmartCom::string appVersion; // 程序版本
 		SmartCom::string devType; // 终端类型
-		int resultCode = GetAppVerAndDevType(communicator, appVersion, devType, readerId);
+		int resultCode = GetAppVerAndDevType(communicator, appVersion, devType);
 		sendData(client, appVersion + "," + devType);
 		return resultCode;
 	} else if (operationName == CString("getChipID")) { // 获取读卡接口芯片ID号
 		SmartCom::string chipID;
-		int resultCode = GetChipID(communicator, chipID, readerId);
+		int resultCode = GetChipID(communicator, chipID);
 		sendData(client, chipID);
 		return resultCode;
 	} else if (operationName == CString("isCardReady")) { // 检测A卡和B卡座是否有卡
 		int cardA = -1; // out, 1,A卡座有卡；0，A卡座无卡
 		int cardB = -1; // out, 1,B卡座有卡；0，B卡座无卡
-		int resultCode = IsCardReady(communicator, cardA, cardB, readerId);
+		int resultCode = IsCardReady(communicator, cardA, cardB);
 		sendData(client, SmartCom::string(i2str(cardA)) + "," + i2str(cardB));
 		return resultCode;
 	} else if (operationName == CString("resetCard")) { // 卡片复位应答
 		SmartCom::string retCode; // out 复位命令的返回值, "F9"：卡座无卡, "FD"：不可识别卡
 		int card = atoi(requestParam[1]); // 1 A卡, 2 B卡
-		int resultCode = ResetCard(communicator, retCode, card, readerId);
+		int resultCode = ResetCard(communicator, retCode, card);
 		if (resultCode != 0)
 		{
 			sendData(client, "ResetCard_wrong");
@@ -161,13 +157,6 @@ int parseCommand(SOCKET client, int readerId, char* command, CString& operationN
 // 		SimpleLog::error("关闭udp通信失败");
 // 		return -102; // 关闭udp通信失败
 // 	}
-
-	if (CloseUDPComm() == -1)
-	{
-		AfxMessageBox("关闭udp通信失败");
-		SimpleLog::error("关闭udp通信失败");
-		return -102; // 关闭udp通信失败
-	}
 
 	return COMMAND_NOT_FOUND; // 命令找不到
 }
