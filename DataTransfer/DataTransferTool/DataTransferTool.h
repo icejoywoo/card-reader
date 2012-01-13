51153622a54af220e86181a7e83cb8463f18c6c5
icejoywoo <icejoywoo@e064f019-fdb2-f8e4-89af-ef8ae8e96563>
2012-01-13 14:52:35 +0000

    很坑爹

---
+++ DataTransfer/DataTransferTool/DataTransferTool.h
@@ -0,0 +1,49 @@
+// DataTransferTool.h : main header file for the DATATRANSFERTOOL application
+//
+
+#if !defined(AFX_DATATRANSFERTOOL_H__E18893F1_84B6_41F9_95AE_86C1CB42A7E6__INCLUDED_)
+#define AFX_DATATRANSFERTOOL_H__E18893F1_84B6_41F9_95AE_86C1CB42A7E6__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#ifndef __AFXWIN_H__
+	#error include 'stdafx.h' before including this file for PCH
+#endif
+
+#include "resource.h"		// main symbols
+
+/////////////////////////////////////////////////////////////////////////////
+// CDataTransferToolApp:
+// See DataTransferTool.cpp for the implementation of this class
+//
+
+class CDataTransferToolApp : public CWinApp
+{
+public:
+	CDataTransferToolApp();
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CDataTransferToolApp)
+	public:
+	virtual BOOL InitInstance();
+	//}}AFX_VIRTUAL
+
+// Implementation
+
+	//{{AFX_MSG(CDataTransferToolApp)
+		// NOTE - the ClassWizard will add and remove member functions here.
+		//    DO NOT EDIT what you see in these blocks of generated code !
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_DATATRANSFERTOOL_H__E18893F1_84B6_41F9_95AE_86C1CB42A7E6__INCLUDED_)
---
+++ DataTransfer/DataTransferTool/resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by DataTransferTool.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_DATATRANSFERTOOL_DIALOG     102
+#define IDR_MAINFRAME                   128
+#define IDC_LIST_TEMPLATES              1001
+#define IDC_LIST_FIELD_TABLE            1002
+#define IDC_BUTTON_NEW_FIELD            1003
+#define IDC_BUTTON_DEL_FIELD            1004
+#define IDC_BUTTON_MODIFY_FIELD         1005
+#define IDC_BUTTON_NEW_TEMPLATE         1006
+#define IDC_BUTTON_DEL_TEMPLATE         1007
+#define IDC_BUTTON_APPLY_TEMPLATE       1008
+#define IDC_BUTTON_EXIT                 1009
+#define IDC_EDIT_TARGET_FILE            1010
+#define IDC_BUTTON_CHOOSE_TARGET        1011
+#define IDC_BUTTON_START_TRANSFER       1013
+#define IDC_EDIT_CURRENT_TEMPLATE       1014
+#define IDC_BUTTON_CHOOSE_DIR           1015
+#define IDC_BUTTON_SAVE_TEMPLATE        1016
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1017
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
