8f6867fce5c8e905692d6b84fba87ffc02a87804
icejoywoo <icejoywoo@e064f019-fdb2-f8e4-89af-ef8ae8e96563>
2012-02-06 03:24:24 +0000

    密钥站文档说明 备份 2012.2.6

---
+++ trunk/backup/密钥站/密钥站客户端动态库/Demo工程/CardReaderClientDllTest/updateDll.bat
@@ -0,0 +1,3 @@
+copy /Y ..\CardReaderClientDll\Release\CardReaderClientDll.dll .
+copy /Y ..\CardReaderClientDll\Release\CardReaderClientDll.lib .
+copy /Y ..\CardReaderClientDll\CardReaderClientDll.h .
