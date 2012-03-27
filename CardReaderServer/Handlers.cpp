d169009c043aaa0ff821425f203a8b256489232b
icejoywoo <icejoywoo@e064f019-fdb2-f8e4-89af-ef8ae8e96563>
2012-03-27 11:52:00 +0000

    知道是什么破原因导致我的服务器不能用了, 无语了

--- CardReaderServer/Handlers.cpp
+++ CardReaderServer/Handlers.cpp
@@ -230,10 +230,12 @@ UINT defaultClientHandler (LPVOID pParam)
 			clientIsQuit = true;
 		}
 		// 接收客户端的请求
-		if (!client->isAvailable() || client->receiveData(buff, 512) == -1 || client->receiveData(buff, 512) == 0) // 接收数据错误即刻关闭
+		int ret = -2;
+		if (!client->isAvailable() || (ret = client->receiveData(buff, 512)) == -1 || ret == 0)// 接收数据错误即刻关闭
 		{
 			break;
 		}
+
 		client->updateTimeout();
 		if ((resultCode= parseCommand(client, readerId, buff, operationName)) >= 0)
 		{
